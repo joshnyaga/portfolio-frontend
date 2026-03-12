461bafbb3adea8cbfe61a286e656df862e5ef4a0
joshnyaga <joshkish55@gmail.com>
2026-03-12 21:14:23 +0300

    adding csp header

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
     default-src 'self';
+    connect-src 'self' https://portfolio-backend.joshuakn.com;
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
